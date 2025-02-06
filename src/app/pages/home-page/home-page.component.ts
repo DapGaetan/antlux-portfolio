e4d76652aa57eedc705b56c18f1080f6d335fd73
Gaëtan <dapvril.gaetan@gmail.com>
2025-02-06 16:27:03 +0100

    refactor: :fire: Changing media folders, adding content and structuring information, creating components

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -5,11 +5,13 @@ import { AboutMeComponent } from '../../about-me/about-me.component';
 import { AchievementsComponent } from '../../achievements/achievements.component';
 import { SkillsComponent } from '../../skills/skills.component';
 import { CareerComponent } from '../../career/career.component';
+import { FooterComponent } from '../../footer/footer.component';
+
 
 
 @Component({
   selector: 'app-home-page',
-  imports: [CareerComponent, SkillsComponent, AchievementsComponent, AboutMeComponent, NavTopComponent, NavLeftComponent],
+  imports: [CareerComponent, SkillsComponent, AchievementsComponent, AboutMeComponent, NavTopComponent, NavLeftComponent, FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
