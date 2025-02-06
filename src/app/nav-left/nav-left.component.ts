012e6cd787b72c3c2c2c29cd4ea8c927ea524c7c
Gaëtan <dapvril.gaetan@gmail.com>
2025-02-06 09:26:58 +0100

    feat: :sparkles: Adding the home page and all its visual components
    -Creation of the NavTop component
    -Creation of the NavLeft component
    -Creation of the AboutMe component
    -Creation of the Achievements component
    -Creation of the Skills component
    -Creation of the Career component
    -Creation of the Footer component

---
+++ src/app/nav-left/nav-left.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-left',
+  imports: [],
+  templateUrl: './nav-left.component.html',
+  styleUrl: './nav-left.component.scss'
+})
+export class NavLeftComponent {
+
+}
