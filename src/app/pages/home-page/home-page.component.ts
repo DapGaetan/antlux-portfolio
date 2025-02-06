ad49ec29830d768fdcc308ed667a64cea40627dc
Gaëtan <dapvril.gaetan@gmail.com>
2025-02-06 09:46:22 +0100

    feat: :sparkles: Bind all visual components to the HomePage component
    -Bind NavTop component
    -Bind of the NavLeft component
    -Bind of the AboutMe component
    -Bind of the Achievements component
    -Bind of the Skills component
    -Bind of the Career component
    -Bind of the Footer component

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { NavLeftComponent } from '../../nav-left/nav-left.component';
+import { NavTopComponent } from '../../nav-top/nav-top.component';
+import { AboutMeComponent } from '../../about-me/about-me.component';
+import { AchievementsComponent } from '../../achievements/achievements.component';
+import { SkillsComponent } from '../../skills/skills.component';
+import { CareerComponent } from '../../career/career.component';
+
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [CareerComponent, SkillsComponent, AchievementsComponent, AboutMeComponent, NavTopComponent, NavLeftComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
