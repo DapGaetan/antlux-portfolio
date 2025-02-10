e24c69eff623ee7bafef326f9d6b99f38466de2a
Gaëtan <dapvril.gaetan@gmail.com>
2025-02-10 11:33:40 +0100

    refactor: :construction: review of how to approach the components and cut out the functionality

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
 import { NavLeftComponent } from '../../nav-left/nav-left.component';
 import { NavTopComponent } from '../../nav-top/nav-top.component';
-import { AboutMeComponent } from '../../about-me/about-me.component';
-import { AchievementsComponent } from '../../achievements/achievements.component';
-import { SkillsComponent } from '../../skills/skills.component';
-import { CareerComponent } from '../../career/career.component';
 import { FooterComponent } from '../../footer/footer.component';
 
 
 
 @Component({
   selector: 'app-home-page',
-  imports: [CareerComponent, SkillsComponent, AchievementsComponent, AboutMeComponent, NavTopComponent, NavLeftComponent, FooterComponent],
+  imports: [NavTopComponent, NavLeftComponent, FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
