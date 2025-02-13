729ed1ef1a60d6c4f2a1bd89a67c6917f7e33aff
Gaëtan <dapvril.gaetan@gmail.com>
2025-02-13 16:52:11 +0100

    style: 💄 Improved css

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ContentComponent } from '../../content/content.component';
 import { NavTopComponent } from '../../nav-top/nav-top.component';
+import { FooterComponent } from '../../footer/footer.component';
 
 
 
 @Component({
   selector: 'app-home-page',
-  imports: [NavTopComponent, ContentComponent],
+  imports: [NavTopComponent, ContentComponent, FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
