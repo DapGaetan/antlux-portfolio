b0918c363b3c057d59787a204c46dbe18c1d32d2
Gaëtan <78353982+DapGaetan@users.noreply.github.com>
2025-02-20 15:53:28 +0100

    style: :lipstick: Improved css

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,6 +2,7 @@ import { MenuComponent } from './../../menu/menu.component';
 import { Component } from '@angular/core';
 import { ContentComponent } from '../../content/content.component';
 import { FooterComponent } from '../../footer/footer.component';
+import { MenuService } from '../../services/menu.service';
 
 
 @Component({
@@ -11,5 +12,16 @@ import { FooterComponent } from '../../footer/footer.component';
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
+  isMenuClose = false;
 
+  constructor(private menuService: MenuService) {
+    // Écoute les changements du menu
+    this.menuService.isMenuClose$.subscribe(state => {
+      this.isMenuClose = state;
+    });
+  }
+
+  toggleMenu(): void {
+    this.menuService.toggleMenu();
+  }
 }
